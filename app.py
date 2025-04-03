29c011076733200e60116463a9ea880b23805c1f
afonso22 <Afonso04dias@gmail.com>
2025-04-03 12:04:22 +0400

    Initial commit: Audibly project

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+from PyPDF2 import PdfReader
+from gtts import gTTS
+import os
+import uuid
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['AUDIO_FOLDER'] = 'audio'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    if 'pdf_file' not in request.files:
+        return "No file part", 400
+
+    file = request.files['pdf_file']
+    if file.filename == '':
+        return "No selected file", 400
+
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+
+    reader = PdfReader(filepath)
+    text = ''
+    for page in reader.pages:
+        text += page.extract_text() + '\n'
+
+    tts = gTTS(text)
+    audio_filename = f"{uuid.uuid4()}.mp3"
+    audio_path = os.path.join(app.config['AUDIO_FOLDER'], audio_filename)
+    tts.save(audio_path)
+
+    return send_file(audio_path, as_attachment=True)
+
+if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    os.makedirs(app.config['AUDIO_FOLDER'], exist_ok=True)
+    app.run(debug=True)
